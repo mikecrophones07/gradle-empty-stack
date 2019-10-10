ef94c2dbce8ca926d23154f13bc9588def8b7dd0
MIKE ANTHONY ECHON (SHP-IRIS-ISD-OOCLL/MNL) <ECHONMI@OOCL.COM>
2019-10-10 14:08:23 +0800

    add validation message

--- src/main/java/Android.java
+++ src/main/java/Android.java
@@ -7,6 +7,10 @@ public class Android extends Mobile{
     @Override
     public void call(String message){
         System.out.print("<Android>");
+        if(message.length() > 25){
+            System.out.println("Message cannot be sent");
+            return;
+        }
         super.call(message);
     }
 
--- src/main/java/Iphone.java
+++ src/main/java/Iphone.java
@@ -6,8 +6,11 @@ public class Iphone extends Mobile {
 
     @Override
     public void call(String message){
-
         System.out.print("<Iphone>");
+        if(message.length() > 25){
+            System.out.println("Message cannot be sent");
+            return;
+        }
         super.call(message);
     }
 

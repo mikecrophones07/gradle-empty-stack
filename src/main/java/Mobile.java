5f73566ac8f0c039becc6710fa6c628b77e17bb1
MIKE ANTHONY ECHON (SHP-IRIS-ISD-OOCLL/MNL) <ECHONMI@OOCL.COM>
2019-10-10 12:10:21 +0800

    add Mobile class and add property and functions

---
+++ src/main/java/Mobile.java
@@ -0,0 +1,9 @@
+public class Mobile {
+    private String name;
+    private String color;
+    private String brand;
+
+    public void call(String message){
+        System.out.println("Message: " + message);
+    }
+}
